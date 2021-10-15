89d2b868a64f5f6989bd49182c1725a93db2ac69
bsprm <92160893+bsprm@users.noreply.github.com>
2021-10-15 17:11:10 +0300

    first upload
    testing

---
+++ code/js/main.js
@@ -0,0 +1,18 @@
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'dark-theme') {
+        setTheme('light-theme');
+    } else {
+        setTheme('dark-theme');
+    }
+}
+
+setTheme('light-theme');
+
+document.querySelector('.switch-btn').addEventListener('click', (e) => {
+    toggleTheme();
+});
