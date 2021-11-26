f0bcb77555b7748111e1c3e725b334b7e3172972
Vlad Berdnikov <92045896+vladbaspromo@users.noreply.github.com>
2021-11-26 14:39:34 +0300

    Показать 2+ сотрудника за одним столом
    Показать 2+ сотрудника за одним столом

--- js/main.js
+++ js/main.js
@@ -13,6 +13,34 @@ function toggleTheme() {
 
 setTheme('light-theme');
 
-document.querySelector('.switch-btn').addEventListener('click', (e) => {
+document.querySelector('.switch').addEventListener('click', (e) => {
     toggleTheme();
+});
+
+if ($(window).width() <= 768) {
+    $('.box-header').click(function () {
+        var current_li = $(this).parent();
+        $('.box-content').each(function (i, el) {
+            if ($(el).parent().is(current_li)) {
+                $(el).prev().toggleClass('is-open');
+                $(el).next().toggleClass('is-open');
+                $(el).slideToggle();
+            } else {
+                $(el).prev().removeClass('is-open');
+                $(el).next().removeClass('is-open');
+                $(el).slideUp();
+            }
+        });
+    });
+}
+
+$('#card-dropdown').click(function () {
+    $(this).toggleClass('is-open');
+});
+
+$(document).on('mouseup', function (e) {
+    let s = $('.card.is-open');
+    if (!s.is(e.target) && s.has(e.target).length === 0) {
+        s.removeClass('is-open');
+    }
 });
